a685d4b11d9011890c7f6e918229f4e41656ae80
Zankhana Patel <zankhanapatel1995.zp@gmail.com>
2019-10-21 17:22:37 -0400

    JPA repository extended

--- SpringBoot2-JPA-CRUD-example/src/main/java/com/demo/sprigboot2/SpringBoot2JPACRUDexample/repository/EmployeeRepository.java
+++ SpringBoot2-JPA-CRUD-example/src/main/java/com/demo/sprigboot2/SpringBoot2JPACRUDexample/repository/EmployeeRepository.java
@@ -2,11 +2,12 @@
  * 
  */
 package com.demo.sprigboot2.SpringBoot2JPACRUDexample.repository;
-
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.demo.springboot2.SpringBoot2JPACRUDexample.model.Employee;
 /**
  * 
  * @author <a href="mailto:z_tel@encs.concordia.ca">Zankhanaben Patel</a>
  */
-public class EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee,Long>{
 
 }
