1488ebae311328ae272c03e9715a03fdf8c34739
Zankhana1995 <zankhanapatel1995.zp@gmail.com>
2020-01-15 14:03:59 -0500

    swagger dependency and config added

--- SpringBoot2-JPA-CRUD-example/src/test/java/com/demo/springboot2/SpringBoot2JPACRUDexample/SpringBoot2JpaCrudExampleApplicationTests.java
+++ SpringBoot2-JPA-CRUD-example/src/test/java/com/demo/springboot2/SpringBoot2JPACRUDexample/SpringBoot2JpaCrudExampleApplicationTests.java
@@ -1,13 +1,13 @@
-package com.demo.springboot2.SpringBoot2JPACRUDexample;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SpringBoot2JpaCrudExampleApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//package com.demo.springboot2.SpringBoot2JPACRUDexample;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class SpringBoot2JpaCrudExampleApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
